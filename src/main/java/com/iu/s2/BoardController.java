c0180575c94e64651d35fae5a5b6db7b29eafa6e
hyonna <yha0118@naver.com>
2019-06-25 17:15:21 +0900

    Initial commit

---
+++ src/main/java/com/iu/s2/BoardController.java
@@ -0,0 +1,63 @@
+package com.iu.s2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.iu.s2.board.BoardVO;
+
+@Controller
+@RequestMapping(value="/notice/**")
+public class BoardController {
+	
+	@RequestMapping(value="noticeDelete", method=RequestMethod.GET)
+	public void boardDelete() {
+		
+		
+	}
+	
+	@RequestMapping(value="noticeSelect", method=RequestMethod.GET)
+	public void boardSelect() {
+		
+		
+	}
+	
+	//리다이렉트
+	@RequestMapping(value="noticeUpdate", method=RequestMethod.POST)
+	public String boardUpdate2() {
+		
+		return "redirect:./boardList";
+	}
+	
+	@RequestMapping(value="noticeUpdate", method=RequestMethod.GET)
+	public void boardUpdate() {
+		
+		
+	}
+	
+	//리다이렉트
+	@RequestMapping(value="noticeWrite", method=RequestMethod.POST)
+	public String boardWrite2(BoardVO boardVO) {
+		
+		System.out.println(boardVO.getNum());
+		System.out.println(boardVO.getTitle());
+		System.out.println(boardVO.getContents());
+		System.out.println(boardVO.getWriter());
+		
+		
+		return "redirect:./boardList";
+	}
+	
+	@RequestMapping(value="noticeWrite", method=RequestMethod.GET)
+	public String boardWrite() {
+		
+		return "notice/noticeWrite";
+	}
+	
+	@RequestMapping(value="noticeList", method=RequestMethod.GET)
+	public void boardList() {
+		
+		
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/board/boardWrite.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style type="text/css">
+
+input {
+
+display: block;
+margin-bottom: 10px;
+padding: 10px 0;
+}
+
+</style>
+</head>
+<body>
+<h1>Board Write</h1>
+
+<form action="./boardWrite" method="post">
+<input type="text" name="num" placeholder="num">
+<input type="text" name="title" placeholder="title">
+<textarea rows="" cols="" name="contents"></textarea>
+<input type="text" name="writer" placeholder="writer">
+<button type="submit">click</button>
+</form>
+
+</body>
+</html>
